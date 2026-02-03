be9bc807df1e0558ed8f94030f4ded68ab24e1d1
neejy-x <tatsmymail@gmail.com>
2026-02-03 20:59:14 +0100

    login service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,9 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
 import { pool } from '../db/pool';
-import type { ResultSetHeader } from 'mysql2/promise';
+import type { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { UserDTO } from '../dto/responses/user.response';
+import { loginInput } from '../validators/auth.validators';
 
 
 interface signupResponse {
@@ -10,6 +11,7 @@ interface signupResponse {
     accessToken: string;
     refreshToken: string;
 }
+
 const secret = String(process.env.JWT_SECRET);
 const refreshSecret = String(process.env.JW_REFRESH_SECRET);
 
@@ -62,7 +64,46 @@ export class AuthService {
     }
     }
 
-    static async login({username, password}: )
+    static async login({username, password}: loginInput):Promise<signupResponse> {
+        //Fetch user from DB
+        const [rows] = await pool.execute<UserDTO[] & RowDataPacket[]>(
+            `SELECT user_id, username, email, role, hashed_password
+            FROM users
+            WHERE username = ?`,
+            [username]
+        )
+        const dbUser = rows[0]
+
+        //Initial check
+        if(!dbUser){
+            const err = new Error('invalid username or password') as Error & { statusCode: number }
+            err.statusCode = 401
+            throw err;
+        }
+        //Destructure to separate the hash from the data we want to return
+        const {hashed_password, ...user} = dbUser
+        user as UserDTO
+        //Verify password
+        const passwordValid = await bcrypt.compare(password, hashed_password)
+        if(!passwordValid){
+            const err = new Error('invalid username  or password') as Error & {statusCode: number}
+            err.statusCode = 401
+            throw err;
+        }
+        //Generate JWTs
+        const accessToken = jwt.sign({userId: user.user_id, role: user.role}, secret, {expiresIn: process.env.JWT_EXPIRES_IN as any})
+        const refreshToken = jwt.sign({userId: user.user_id, role: user.role}, refreshSecret, {expiresIn: process.env.JWT_REFRESH_EXPIRES_IN as any})
+
+        //insert token into db
+        await pool.execute<ResultSetHeader>(
+            `INSERT INTO refresh_tokens(user_id, token)
+            VALUES (?, ?)`,
+            [user.user_id, refreshToken]
+        )
+
+        return{ user, accessToken, refreshToken}
+    
+    }
 
     static async logout(refreshToken: string){
         try{
