446a0702e3389f23f22ea0dcfbd172ccc8bb0fd3
neejy-x <tatsmymail@gmail.com>
2026-02-03 21:20:21 +0100

    login route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import type { Router } from 'express';
-import { logoutHandler, signUpHandler } from '../controllers/auth.controller';
+import { loginHandler, logoutHandler, signUpHandler } from '../controllers/auth.controller';
 import { validate } from '../middlewares/validator.middleware';
-import { signUpSchema } from '../validators/auth.validators';
+import { loginSchema, signUpSchema } from '../validators/auth.validators';
 
 export const authRouter:Router = express.Router()
 
 authRouter.post('/signup', validate(signUpSchema), signUpHandler)
+authRouter.post('/login', validate(loginSchema), loginHandler)
 authRouter.post('/logout', logoutHandler)
