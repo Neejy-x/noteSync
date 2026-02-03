03e923e12000b2f1fd2638479017bd6c056435d4
neejy-x <tatsmymail@gmail.com>
2026-02-03 21:11:34 +0100

    login handler

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { AuthService } from '../services/auth.service';
 import { UserDTO } from '../dto/responses/user.response';
-import { SignUpInput } from '../validators/auth.validators';
+import { loginInput, SignUpInput } from '../validators/auth.validators';
 
 
 
@@ -24,6 +24,30 @@ export const signUpHandler = async (
         },
         accessToken
     })
+}
+
+export const loginHandler = async(
+    req: Request<{}, {}, loginInput>, 
+    res:Response<{Success: boolean, message: string, data: UserDTO, accessToken: string}>
+) => {
+    const {username, password} = req.body
+    const {user, accessToken, refreshToken} = await AuthService.login({username, password})
+
+    res.cookie('jwt', refreshToken,
+        {
+            httpOnly: true,
+            secure: process.env.NODE_ENV == 'production',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        }
+    )
+
+    res.status(201).json({
+        Success: true,
+        message: 'Login Successful',
+        data: user,
+        accessToken
+    })
+
 }
 
 export const logoutHandler = async(req:Request, res:Response<{Success: boolean, message: string}>) => {
