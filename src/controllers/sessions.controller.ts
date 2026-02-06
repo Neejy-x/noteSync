e3b831f9315aa71b0fd133e9513aa69baf369a12
neejy-x <tatsmymail@gmail.com>
2026-02-06 12:43:45 +0100

    getsessions controller

--- src/controllers/sessions.controller.ts
+++ src/controllers/sessions.controller.ts
@@ -2,5 +2,17 @@ import type {Request, Response} from 'express'
 import { AuthService } from '../services/auth.service'
 
 
-
+export const sessionsHandler = (req: Request, res: Response<{Success: boolean, message: string, data?: {} }>) => {
+     
+    if(!req.user){
+        return res.status(401).json({Success: false, message: 'Unauthorized: user not authenticated'})
+    }
+    const {user_id} = req.user
+    
+    const sessions = AuthService.getSessions(user_id)
+    res.status(200).json({
+        Success: true,
+        message: 'Active sessions',
+        data: sessions
+    })
 }
