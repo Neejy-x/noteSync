6c32c23d8c8de2512fa0e5d8deb3a3bacb3a1951
neejy-x <tatsmymail@gmail.com>
2026-02-06 21:30:36 +0100

    create note route

--- src/routes/notes.routes.ts
+++ src/routes/notes.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllNotes, getNoteById} from '../controllers/notes.controller'
+import { getAllNotes, getNoteById, createNoteHandler} from '../controllers/notes.controller'
 import {authenticate} from '../middlewares/auth.middlewares'
 
 export const noteRouter = express.Router();
@@ -9,3 +9,4 @@ noteRouter.use(authenticate)
 
 noteRouter.get('/', getAllNotes)
 noteRouter.get('/:noteId', getNoteById)
+noteRouter.post('/', createNoteHandler)
