3ded986e1087c010f8b2ae2acad7b101521f7426
neejy-x <tatsmymail@gmail.com>
2026-02-06 16:16:41 +0100

    session router  and validator

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { Router } from 'express'
 import { authenticate, isAdmin } from '../middlewares/auth.middlewares'
-import { getSessionsHandler } from '../controllers/sessions.controller'
+import { endSessionHandler, getSessionsHandler } from '../controllers/sessions.controller'
+import { sessionIdValidator } from '../validators/session.validators'
+import { validate } from '../middlewares/validator.middleware'
 
 const sessionsRouter: Router = express.Router()
 
 sessionsRouter.get('/', authenticate, getSessionsHandler)
-sessionsRouter.post('/end/:sessionId', authenticate, )
+sessionsRouter.post('/end/:sessionId', validate(sessionIdValidator), authenticate, endSessionHandler)
